12efe0f861efd09c8cb53b42ab9151aac64e87c4
Arthals <zhuozhiyongde@126.com>
2023-01-27 00:08:45 +0800

    Optimize the process

--- TreeholeNotifier.py
+++ TreeholeNotifier.py
@@ -358,7 +358,7 @@ if __name__ == "__main__":
     # get uid and password from args
     if (len(sys.argv) != 3):
         print("Usage: python3 treehole_updater.py uid password")
-        exit(0)
+        exit(1)
     uid = sys.argv[1]
     password = sys.argv[2]
 
@@ -372,8 +372,7 @@ if __name__ == "__main__":
     keywords_update_list = updater.check_update_with_keywords()
     if (len(treehole_update_list) == 0 and len(keywords_update_list) == 0):
         log("No update")
-        exit(0)
+        exit(1)
     else:
         log("Update detected")
-        with open("flag.txt", "w") as f:
-            f.write("Higher Grade!")
+        exit(0)
